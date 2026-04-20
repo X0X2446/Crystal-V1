79f783cbab3259a19afa07d5fb707cf87ea2563b
STEROM <158181281+X0X2446@users.noreply.github.com>
2026-04-20 15:00:01 -0500

    Update service worker to use scramjet.worker.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,7 @@
-importScripts("/scram/scramjet.all.js");
+importScripts('/scramjet/scramjet.worker.js');
 
-const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
-}
-
-self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(scramjet.fetch(event));
 });
