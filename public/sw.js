399d32262ec75552417208f32ce8792d1bc92378
STEROM <158181281+X0X2446@users.noreply.github.com>
2026-04-21 08:54:30 -0500

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('/scramjet/scramjet.worker.js');
-
-const scramjet = new ScramjetServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('/scramjet/')) {
-    event.respondWith(scramjet.fetch(event));
-  }
-});
